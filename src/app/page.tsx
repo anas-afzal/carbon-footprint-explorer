16693a25a3228223709a80ef4d9e7684adde09ee
anas-afzal <muhammad.afzal@code.berlin>
2024-07-01 23:15:48 +0200

    style: delete duplicates files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = (): JSX.Element => {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-10'>
-      <h1 className='text-4xl font-bold'>Carbon Footprint Explorer</h1>
-    </main>
-  );
-};
-
-export default Home;
