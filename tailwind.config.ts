2fc9952dc450a565383e3668e0edb03a631eb841
anas-afzal <muhammad.afzal@code.berlin>
2024-07-01 22:30:15 +0200

    chore(tailwind.config.ts): add extra colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        primary: { 100: '#40D7A1', 200: '#1BAA77' },
+        'custom-white': '#F7F5F2',
+        'custom-grey': {
+          100: '#949691',
+          200: '#626660',
+        },
+        secondary: '#30362F',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
